a8c23c511ddafdd67406e8b0ec807abc0b452b17
BoudBoud47 <mehdi.boudart@gmail.com>
2019-11-27 21:47:47 +0100

    Counter of request

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,17 @@ struct AppState {
     app_name: String,
 }
 
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+}
+
+fn _index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {}", counter) // <- response with count
+}
+
 fn index(data: web::Data<AppState>) -> String {
     let app_name = &data.app_name; // <- get app_name
 
@@ -26,8 +37,14 @@ fn index3() -> impl Responder {
 fn main() {
     println!("Hello, world!");
     let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+    let mut server = HttpServer::new(move || {
+        // move counter into the closure
         App::new()
+            .register_data(counter.clone())
+            .route("/2", web::get().to(_index)) // <- register the created data
             .data(AppState {
                 app_name: String::from("Actix-web"),
             })
