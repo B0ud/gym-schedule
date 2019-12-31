40fe7bd733595230fa2d04711e5fb588cc44a669
BoudBoud47 <mehdi.boudart@gmail.com>
2019-12-31 10:58:30 +0100

    App run migration

--- src/config/db_config.rs
+++ src/config/db_config.rs
@@ -1,14 +1,19 @@
-use diesel::{r2d2::ConnectionManager, PgConnection};
+use diesel::{r2d2::ConnectionManager, Connection, PgConnection};
 use std::env;
 
 // type alias to use in multiple places
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
-pub fn manage_database() -> Pool {
+pub fn establish_connection_pool() -> Pool {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL 00 must be set");
     info!("Db connection pool : {}", database_url);
     let manager = ConnectionManager::<PgConnection>::new(database_url);
     r2d2::Pool::builder()
         .build(manager)
         .expect("Failed to create pool.")
+}
+
+pub fn establish_connection() -> PgConnection {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL 00 must be set");
+    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
